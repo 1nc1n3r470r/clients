bd30ed6ad0945f43f1adbe72dbb5e2b05ab3062d
Daniel García <dani-garcia@users.noreply.github.com>
2023-09-28 13:27:53 +0200

    [PM-3686] Remove ipcRenderer from native-message-handler

--- apps/desktop/src/platform/preload.ts
+++ apps/desktop/src/platform/preload.ts
@@ -1,7 +1,9 @@
 import { ipcRenderer } from "electron";
 
 import { DeviceType } from "@bitwarden/common/enums/device-type.enum";
+import { EncString } from "@bitwarden/common/platform/models/domain/enc-string";
 
+import { EncryptedMessageResponse, UnencryptedMessageResponse } from "../models/native-messaging";
 import { isDev, isWindowsStore } from "../utils";
 
 export default {
@@ -12,6 +14,15 @@ export default {
   isDev: isDev(),
   isWindowsStore: isWindowsStore(),
   reloadProcess: () => ipcRenderer.send("reload-process"),
+
+  sendNativeMessagingReply: (
+    message:
+      | EncryptedMessageResponse
+      | UnencryptedMessageResponse
+      | { appId: string; command?: string; sharedSecret?: string; message?: EncString }
+  ) => {
+    ipcRenderer.send("nativeMessagingReply", message);
+  },
 };
 
 function deviceType(): DeviceType {
